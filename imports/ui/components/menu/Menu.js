2cd217461c1d89afdb3154de227882c8680e4f91
Maksim Gryaznov <Maksim.Gryaznov@rossko.ru>
2018-05-01 18:19:17 +0300

    error fix

--- imports/ui/components/menu/Menu.js
+++ imports/ui/components/menu/Menu.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux';
 import { array, func } from 'prop-types';
 import { callGetMenu } from '../../../api/redux/async-actions';
 
-const Menu = ({ menu, fetch }) =>
-    fetch() && <div className="menu">{menu.map((item, index) => <div key={index}>{item.name}</div>)}</div>;
+const Menu = ({ menu, fetch }) => {
+    fetch();
+
+    return <div className="menu">{menu.map((item, index) => <div key={index}>{item.name}</div>)}</div>;
+};
 
 Menu.propTypes = {
     menu: array.isRequired,
     fetch: func.isRequired
 };
-
-// Menu.defaultProps = { menu: [] };
 
 const mapStateToProps = state => ({ menu: state.menu });
 
