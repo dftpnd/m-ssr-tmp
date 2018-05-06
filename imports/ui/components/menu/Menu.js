152c6ee06eafc85fc08dfe27e6e2391e96c41147
Noris <noris75564@gmail.com>
2018-05-06 09:05:00 +0300

    11

--- imports/ui/components/menu/Menu.js
+++ imports/ui/components/menu/Menu.js
@@ -6,11 +6,52 @@ import { callGetMenu } from '../../../api/redux/async-actions';
 const Menu = ({ menu, fetch }) => {
     fetch();
 
+    const menu2 = [
+        {
+            name: 'pizza',
+            list: [
+                {
+                    name: 'Boscaoila',
+                    price: '230'
+                },
+                {
+                    name: 'Primavera',
+                    price: '300'
+                }
+            ]
+        },
+        {
+            name: 'паста',
+            list: [
+                {
+                    name: 'Kolya',
+                    price: '130'
+                },
+                {
+                    name: 'Sergei',
+                    price: '500'
+                }
+            ]
+        }
+    ];
+
     return (
-        <div className="menu">
-            {menu.map((item, index) => <div key={index}>{item.name}</div>)}
-            <a>мудак</a>
-        </div>
+        <section>
+            <div className="menu">
+                {menu.map((item, index) => <div key={index}>{item.name}</div>)}
+                <a>мудак</a>
+            </div>
+            {menu2.map((item, index) => {
+                return (
+                    <div key={index}>
+                        {item.list.map((subItem, i) => {
+                            return <div key={i}>{subItem.name}</div>;
+                        })}
+                    </div>
+                );
+            })}
+            asdasdas
+        </section>
     );
 };
 
