cdabbd8ef1dad8d3dd6658359d9c9eb7ac78d8db
Noris <noris75564@gmail.com>
2018-05-07 14:07:52 +0300

    begin

--- imports/ui/components/menu/Menu.js
+++ imports/ui/components/menu/Menu.js
@@ -11,26 +11,110 @@ const Menu = ({ menu, fetch }) => {
             name: 'pizza',
             list: [
                 {
-                    name: 'Boscaoila',
-                    price: '230'
+                    name: 'Boscaiola (Боскайола)',
+                    price: '410'
                 },
                 {
-                    name: 'Primavera',
-                    price: '300'
+                    name: 'Primavera (Примавера)',
+                    price: '450'
+                },
+                {
+                    name: 'Soleluna (Солелуна)',
+                    price: '420'
+                },
+                {
+                    name: 'A modo mio (А модо мио)',
+                    price: '500'
+                },
+                {
+                    name: 'Gustosa (Густоза)',
+                    price: '500'
+                },
+                {
+                    name: 'Vegetariana (Веджетариана)',
+                    price: '350'
+                },
+                {
+                    name: 'Diavola (Дьявола)',
+                    price: '370'
+                },
+                {
+                    name: 'Dolce Verona (Дольче Верона)',
+                    price: '520'
+                },
+                {
+                    name: 'Friulana (Фриулана)',
+                    price: '400'
+                },
+                {
+                    name: 'Bolognese (Болоньезе)',
+                    price: '390'
                 }
+                {
+                    name: 'La metà (Ла мета)',
+                    price: '450'
+                },
+                {
+                    name: 'Margherita (Маргерита)',
+                    price: '450'
+                },
+                {
+                    name: 'Calabrese (Калабрэзэ)',
+                    price: '350'
+                },
+                {
+                    name: 'Tartufo (Тартуфо)',
+                    price: '450'
+                },
+                {
+                    name: '4 formaggi (4 формаджи)',
+                    price: '520'
+                },
+                {
+                    name: 'Napoletana (Наполетана)',
+                    price: '750'
+                },
             ]
         },
         {
-            name: 'паста',
+            name: 'Pasta',
             list: [
                 {
-                    name: 'Kolya',
-                    price: '130'
+                    name: 'Tagliatelle alla boscaiola (Тальятелле алла боскайола)',
+                    price: '350'
                 },
                 {
-                    name: 'Sergei',
-                    price: '500'
-                }
+                    name: 'Al pomodoro (Аль помодоро)',
+                    price: '320'
+                },
+                {
+                    name: 'Bolognese (Болоньезе)',
+                    price: '320'
+                },
+                {
+                    name: 'Penne con pollo (Пенне кон полло)',
+                    price: '300'
+                },
+                {
+                    name: 'Piemontese (Пьемонтезе)',
+                    price: '420'
+                },
+                {
+                    name: 'Carbonara (Карбонара)',
+                    price: '350'
+                },
+                {
+                    name: 'Nera (Нера)',
+                    price: '380'
+                },
+                {
+                    name: 'Tenera (Тэнэра)',
+                    price: '450'
+                },
+                {
+                    name: 'Con calamari',
+                    price: '450'
+                },
             ]
         }
     ];
